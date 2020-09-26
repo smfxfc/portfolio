8162d0d4863f25c0753f6cb427dd3ad3b5ca9821
smf17 <smf17@protonmail.com>
2020-09-26 18:23:21 -0400

    Added daily high, low, dividend tracking and implemented dividend reinvestment

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,7 @@
 #! python3
-# portfolio.py - Pull stock data from an API and update portfolio tracker accordingly
-# the script will run once a day after market close in order to capture and save a snapshot of the portfolio's value on any date whilst script was active 
+# portfolio.py - Pull stock data from yfinance API and update portfolio tracker accordingly
+# the script will run once a day after market close
+
 import sys
 from datetime import datetime
 
@@ -19,29 +20,39 @@ destination = openpyxl.load_workbook(portfolio_history)
 
 current = wb['Current']
 
-# copy 'total market value' cell in to a new cell and store it as 'yesterday's portfolio value'. This will be implemented before the cells are updated to reflect the current day's market holdings, so that after current prices are updated I can calculate today's +- by taking subtracting the two.  
-current.cell(row=2, column=13).value = current.cell(row=7, column=8).value
+# store prior day's market value before updating for current day's prices
+current.cell(row=2, column=16).value = current.cell(row=7, column=8).value
 
 # Loop through rows to grab ticker references which will be used to pull stock data from yf
 for row_num in range(2, current.max_row): # skipping first row because it's the header. leaving last row because it's the total
     stock_ticker = (current.cell(row=row_num, column=2)).value
-    print(stock_ticker)
+    print(f"Gathering {stock_ticker} data.")
     stock_ticker_string = str(stock_ticker)
     stock_info = yf.Ticker(stock_ticker_string)
-    print(stock_info)
     stock_data = stock_info.history(period = '5d')
     current_quote = (stock_data.tail(1)['Close'].iloc[0])
     yesterday_quote = (stock_data.tail(2)['Close'].iloc[0])
+    day_high = (stock_data.tail(1))['High'].iloc[0]
+    day_low = (stock_data.tail(1))['Low'].iloc[0]
+    day_dividends = (stock_data.tail(1))['Dividends'].iloc[0]
+
     current.cell(row=row_num, column=7).value = current_quote
     current.cell(row=row_num, column=3).value = (current_quote - yesterday_quote)/yesterday_quote
     current.cell(row=row_num, column=8).value = current.cell(row=row_num, column=7).value * current.cell(row=row_num, column = 4).value
     current.cell(row=row_num, column=9).value = (current.cell(row=row_num, column=8).value - current.cell(row=row_num, column=6).value)
     current.cell(row=row_num, column=10).value = current.cell(row=row_num, column=9).value / current.cell(row=row_num, column=6).value * 100
+    current.cell(row=row_num, column=11).value = day_high
+    current.cell(row=row_num, column=12).value = day_low
+    current.cell(row=row_num, column=13).value = day_dividends * current.cell(row=row_num, column=4).value
 
-# calculate today's portfolio +-  
+    if day_dividends:
+        print(f"Recording reinvestment of {stock_ticker} dividend payment.")
+        current.cell(row=row_num, column=4).value += current.cell(row=row_num, column=13).value / current_quote
+        current.cell(row=row_num, column=6).value += current.cell(row=row_num, column=13).value
+
 print("Calculating today's gain/loss...")
 current.cell(row=7, column=8).value = current.cell(row=2, column=8).value + current.cell(row=3, column=8).value + current.cell(row=4, column=8).value + current.cell(row=5, column=8).value + current.cell(row=6, column=8).value
-current.cell(row=1, column=13).value = round(float(current.cell(row=7, column=8).value) - float(current.cell(row=2, column=13).value),2)
+current.cell(row=1, column=16).value = round(float(current.cell(row=7, column=8).value) - float(current.cell(row=2, column=16).value),2)
 
 # create tab in portfolio_history.xlsx file to store the day's portfolio value,
 # naming the tabs as the current date
